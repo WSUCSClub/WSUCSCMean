d1671b29d48ccbd32c54da759b4aaf8122a38e84
Malek Hakim <hakimelek@gmail.com>
2015-08-21 16:39:35 -0500

    first commit

---
+++ app/models/member.server.model.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+
+var validateLocalStrategyProperty = function(property) {
+	return ((this.provider !== 'local' && !this.updated) || property.length);
+};
+
+/**
+ * Member Schema
+ */
+var MemberSchema = new Schema({
+	firstName: {
+		type: String,
+		default: '',
+		required: 'Please fill Member first name',
+		trim: true
+	},
+	lastName: {
+		type: String,
+		default: '',
+		required: 'Please fill Member first name',
+		trim: true
+	},
+	Status: {
+		type: String,
+		default: 'Active Member',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	email: {
+		type: String,
+		trim: true,
+		default: '',
+		validate: [validateLocalStrategyProperty, 'Please fill in your email'],
+		match: [/.+\@.+\..+/, 'Please fill a valid email address']
+	},
+	username: {
+		type: String,
+		unique: 'testing error message',
+		required: 'Please fill in a username',
+		trim: true
+	},
+
+	status: {
+		type: [{
+			type: String,
+			enum: ['Member','Past Member','Active Member', 'Secretary', 'Public Relations Responsible','Project Committee Responsible','Vice-president','Faculty','President', 'Treasurer', 'Membership Committee responsible']
+		}],
+		default: ['Member']
+	},
+	phone: {
+		type: String,
+		default: '',
+		required: 'Please fill your phone number',
+		trim: true
+	},
+	committee: {
+		type: [{
+			type: String,
+			enum: ['MC', 'PDC', 'PC']
+		}],
+		default: ['MC']
+	},
+	class: {
+		type: [{
+			type: String,
+			enum: ['CS101', 'CS234', 'CS250', 'CS275','CS313', 'CS341']
+		}],
+		default: ['']
+	},
+});
+
+mongoose.model('Member', MemberSchema);
---
+++ public/modules/core/controllers/home.client.controller.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+angular.module('core').controller('HomeController', ['$scope', 'Authentication',
+	function($scope, Authentication) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+
+		$scope.alerts = [
+			{
+				icon: 'glyphicon-user' ,
+				color:'btn-primary' ,
+				total: '35' ,
+				description: 'TOTAL MEMBERS'
+			},
+
+			{
+				icon: 'glyphicon-blackboard' ,
+				color:'btn-info' ,
+				total: '5' ,
+				description: 'TOTAL PROJECTS'
+			},
+			{
+				icon: 'glyphicon-star' ,
+				color:'btn-warning' ,
+				total: '3' ,
+				description: 'TOTAL AWARDS'
+			},
+
+			{
+				icon: 'glyphicon-align-left' ,
+				color:'btn-warning' ,
+				total: '1204' ,
+				description: 'TOTAL POSTS'
+			},
+
+			{
+				icon: 'glyphicon-calendar' ,
+				color:'btn-success' ,
+				total: '3' ,
+				description: 'UPCOMING EVENTS'
+			},
+
+			{
+				icon: 'glyphicon-phone' ,
+				color:'btn-primary' ,
+				total: '11' ,
+				description: 'TOTAL APPS DEVELOPED'
+			}
+
+		];
+	}
+]);
